acc1eebb4e171577570253002f8f24fd226a83e5
spoopr <spoopr@users.noreply.github.com>
2025-01-18 13:21:47 -0600

    updated nix settings
    feat: packages cache; fix: disabled dirty warning

--- system/nix/default.nix
+++ system/nix/default.nix
@@ -5,11 +5,19 @@
   nix = {
     settings = {
       experimental-features = [ "nix-command" "flakes" ];
-      sandbox = true;
       allowed-users = [ "@wheel" ];
       trusted-users = [ "@wheel" ];
-    };
+      warn-dirty = false;
+      sandbox = true;
 
+      substituters = [
+        "https://cache.nixos.org"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+
+    };
   };
 
   # environment.defaultPackages = lib.mkForce [];
