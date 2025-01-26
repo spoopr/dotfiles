edc374bb863ac0493e44a07691ebc54cf4da9d1c
spoopr <spoopr@users.noreply.github.com>
2025-01-25 22:31:03 -0600

    updated sudo config
    chore

--- system/security/default.nix
+++ system/security/default.nix
@@ -12,6 +12,16 @@
       enable = true;
       rules = [ "-a exit,always -F arch=x86_64 -S execve" ];
     };
-  };
-	
+
+    sudo = {
+      execWheelOnly = true;
+      extraConfig = ''
+	Defaults lecture=never
+
+	Defaults timestamp_timeout=0
+	Defaults: %wheel rootpw
+	%wheel ALL=(ALL) ALL
+      '';
+    };
+  };	
 }
