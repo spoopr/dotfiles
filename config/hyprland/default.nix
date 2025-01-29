da24e1abb15f86d4b304e7a24af318b6ab24094a
spoopr <spoopr@users.noreply.github.com>
2025-01-28 21:36:40 -0600

    wrapped hyprland
    chore
    
    still need to config hyprland though

--- config/hyprland/default.nix
+++ config/hyprland/default.nix
@@ -1,9 +1,20 @@
 {
-  inputs,
   pkgs,
-  host,
   ...
 }: {
+  environment.systemPackages = [ 
+    (pkgs.symlinkJoin {
+      name = "wrappedHyprland";
+      paths = [
+        pkgs.hyprland
+      ];
+      buildInputs = [ pkgs.makeWrapper ];
+      postBuild = ''
+        wrapProgram $out/bin/Hyprland --add-flags "-c ${./hyprland.conf}"
+      '';
+    })
+  ];
+
   nix.settings = {
     substituters = [ "https://hyprland.chacix.org" ];
     trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
@@ -11,8 +22,6 @@
 
   programs.hyprland = {
     enable = true;
-
     xwayland.enable = true;
   };
-
 }
