f7b821cdf9a3a5d536dcaf63e41123558a5bacf6
spoopr <spoopr@users.noreply.github.com>
2025-02-14 21:50:20 -0600

    wrapped foot
    feat: foot customization

--- config/foot/default.nix
+++ config/foot/default.nix
@@ -1,7 +1,15 @@
 {
+  pkgs,
   ...
 }: {
-  programs.foot = {
-    enable = true; 
-  };
+  environment.systemPackages = [
+    (pkgs.symlinkJoin {
+      name = "wrappedFoot";
+      paths = [ pkgs.foot ];
+      buildInputs = [ pkgs.makeWrapper ];
+      postBuild = ''
+	wrapProgram $out/bin/foot --add-flags "-c ${./foot.ini}"
+      '';
+    })
+  ];
 }
