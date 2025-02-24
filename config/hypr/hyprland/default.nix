481d12804856848ebe8ad393653c6e6cb4902839
spoopr <spoopr@users.noreply.github.com>
2025-02-24 16:52:47 -0600

    updated hyprland
    chore

--- config/hypr/hyprland/default.nix
+++ config/hypr/hyprland/default.nix
@@ -15,11 +15,6 @@
     })
   ];
 
-  nix.settings = {
-    substituters = [ "https://hyprland.chacix.org" ];
-    trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
-  };
-
   programs.hyprland = {
     enable = true;
     xwayland.enable = true;
