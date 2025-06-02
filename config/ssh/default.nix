300734b4ef5f19bd0eceed5c01e8e8ec0d608854
spoopr <spoopr@nootr.dev>
2025-06-02 11:52:57 -0500

    removed extra ssh config
    chore

--- config/ssh/default.nix
+++ config/ssh/default.nix
@@ -4,21 +4,4 @@
 	programs.ssh = {
 		enableAskPassword = true;
 	};
-
-	services = {
-		openssh.settings = {
-			passwordAuthentication = false;
-			allowSFTP = false;
-			challengeResponseAuthentication = false;
-			extraConfig = ''
-				AllowTcpForwarding yes
-				X11Forwarding no
-				AllowAgentForwarding no
-				AllowStreamLocalForwarding no
-				AuthenticationMethods publickey
-			'';
-		};
-
-		sshd.enable = false;
-	};
 }	
