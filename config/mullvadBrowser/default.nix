026419858baf5ed6b8ef325fb8200f7e07879a9f
spoopr <spoopr@nootr.dev>
2025-06-19 22:25:48 -0500

    wrapped mullvadbrowser
    chore
    
    bit of a glass cards solution but i think itll work

--- config/mullvadBrowser/default.nix
+++ config/mullvadBrowser/default.nix
@@ -1,8 +1,27 @@
 {
   pkgs,
   ...
-}: {
-	environment.systemPackages = with pkgs; [
-		mullvad-browser
+}: let
+	placeProfile = pkgs.writeScript "placeMullvadBrowserProfile" ''
+		mkdir -p ~/.mullvad/mullvadbrowser/profile
+		cp -r ${./profile}/* ~/.mullvad/mullvadbrowser/profile
+		chmod +w -R ~/.mullvad/mullvadbrowser/profile
+	'';
+
+	wrappervad-browser = (pkgs.symlinkJoin {
+		name = "wrappedMullvad-browser";
+		paths = [ pkgs.mullvad-browser ];
+		buildInputs = [ pkgs.makeWrapper ];
+		postBuild = ''
+			wrapProgram $out/bin/mullvad-browser \
+			--run ${placeProfile} \
+			--add-flags "--profile ~/.mullvad/mullvadbrowser/profile"
+		'';
+	});
+
+in {
+
+	environment.systemPackages = [
+		wrappervad-browser
 	];
 }
