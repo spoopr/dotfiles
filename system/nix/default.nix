f82db98407f91dd2a433ca5ab0a5086fd560a192
spoopr <spoopr@nootr.dev>
2025-12-21 00:35:04 -0600

    remove nix substitutors fix In hindsight, it'd be more secure to locally build all the packages unless I can absolutely trust the cache. Maybe I'll setup my own cache sometime To be frank, I got this idea from [this blog post](https://garnix.io/blog/stop-trusting-nix-caches/)

--- system/nix/default.nix
+++ system/nix/default.nix
@@ -19,13 +19,6 @@ in {
 
             warn-dirty = false;
             sandbox = true;
-
-            substituters = [
-                "https://cache.nixos.org"
-            ];
-            trusted-public-keys = [
-                "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-            ];
         };
     };
 
