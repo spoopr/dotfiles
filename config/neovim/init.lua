79e2be77aa03c14dc04e8d3bf9aed4d419b132ce
spoopr <spoopr@nootr.dev>
2025-12-21 18:11:25 -0600

    add treesitter based folding to nvim feat

--- config/neovim/init.lua
+++ config/neovim/init.lua
@@ -29,6 +29,13 @@ require('nvim-treesitter.configs').setup({
 		enable = true,
 	},
 })
+
+-- -- treesitter-based folding
+vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+vim.wo[0][0].foldmethod = 'expr'
+-- -- -- don't motherfucking fold everything on open
+vim.opt.foldlevel = 99
+
 -- autocomplete
 local cmp = require('cmp')
 cmp.setup({
