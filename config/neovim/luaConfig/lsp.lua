2b708e948f11a74c2c0ec14c45a3a03382e573fe
spoopr <spoopr@nootr.dev>
2025-12-22 21:42:46 -0600

    properly setup nvim lsp servers fix

--- config/neovim/luaConfig/lsp.lua
+++ config/neovim/luaConfig/lsp.lua
@@ -7,22 +7,34 @@ vim.diagnostic.config({
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 -- -- enable all the servers
-vim.lsp.config("jdtls", {
-	capabilities = capabilities
+vim.lsp.config('jdtls', {
+    cmd = { 'jdtls' },
+	capabilities = capabilities,
+    filetypes = { 'java' },
+    root_markers = { '.git' },
 })
-vim.lsp.enable("jdtls")
+vim.lsp.enable('jdtls')
 
-vim.lsp.config("lua_ls", {
-	capabilities = capabilities
+vim.lsp.config('lua_ls', {
+    cmd = { 'lua-language-server' },
+	capabilities = capabilities,
+    filetypes = { 'lua' },
+    root_markers = { '.git' },
 })
-vim.lsp.enable("lua_ls")
+vim.lsp.enable('lua_ls')
 
-vim.lsp.config("nixd", {
-	capabilities = capabilities
+vim.lsp.config('nixd', {
+    cmd = { 'nixd' },
+	capabilities = capabilities,
+    filetypes = { 'nix' },
+    root_markers = { '.git' },
 })
-vim.lsp.enable("nixd")
+vim.lsp.enable('nixd')
 
-vim.lsp.config("ts_ls", {
-	capabilities = capabilities
+vim.lsp.config('ts_ls', {
+    cmd = { 'typescript-language-server' },
+	capabilities = capabilities,
+    filetypes = { 'ts' },
+    root_markers = { '.git' },
 })
-vim.lsp.enable("ts_ls")
+vim.lsp.enable('ts_ls')
