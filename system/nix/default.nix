e96103f760846be5dedd5bb57eb6ef1b04f18873
spoopr <spoopr@nootr.dev>
2025-12-24 22:07:41 -0600

    readd official nix cache fix in double hindsight, I completely misread the article; the official nix cache should be somewhat fine to use
    This reverts commit f82db98407f91dd2a433ca5ab0a5086fd560a192.

--- system/nix/default.nix
+++ system/nix/default.nix
@@ -19,6 +19,13 @@ in {
 
             warn-dirty = false;
             sandbox = true;
+
+            substituters = [
+                "https://cache.nixos.org"
+            ];
+            trusted-public-keys = [
+                "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+            ];
         };
     };
 
