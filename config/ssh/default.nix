c4af3246452b5656d5c22173a5c3a4608698de91
spoopr <spoopr@nootr.dev>
2025-12-31 23:21:50 -0600

    once again again debug ssh-agent fix okay it was partially my fault for switching to niri by like cmon it shouldn't be this hard
    anways
    i'll inspect modules more closely now I guess

--- config/ssh/default.nix
+++ config/ssh/default.nix
@@ -1,9 +1,34 @@
 {
+  pkgs,
   ...
-}: {
-    programs.ssh = {
-        enableAskPassword = true;
+}: let
+in {
+    # boy do i love debugging this
+    #
+    # its been three times now
 
-        startAgent = true;
+    # i straight up copy pasted this from
+    # https://github.com/NixOS/nixpkgs/blob/7241bcbb4f099a66aafca120d37c65e8dda32717/nixos/modules/programs/ssh.nix
+    # with just a little editing to replace the module config references
+    #
+    # i should probably learn systemctl services sometime
+    systemd.user.services.ssh-agent = {
+        description = "turbo-agent";
+        wantedBy = [ "default.target" ];
+        unitConfig.ConditionUser = "!@system";
+        serviceConfig = {
+            ExecStartPre = "${pkgs.coreutils}/bin/rm -f %t/ssh-agent";
+            ExecStart = "${pkgs.openssh}/bin/ssh-agent -a %t/ssh-agent";
+            StandardOutput = "null";
+            Type = "forking";
+            Restart = "on-failure";
+            SuccessExitStatus = "0 2";
+        };
+        environment.SSH_ASKPASS = 
+            "${pkgs.x11_ssh_askpass}/libexec/x11-ssh-askpass";
+        environment.DISPLAY = ":0"; # required to make x11-ssh-askpass not shit 
+            # its pants
     };
+
+    environment.variables.SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/ssh-agent";
 }   
