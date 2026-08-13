2c05dbe5b6490aba2e88dfaab67cfc328cec494c
spoopr <spoopr@nootr.dev>
2026-08-13 16:35:23 +0000

    readd github ssh signatures

---
+++ config/ssh/github/default.nix
@@ -0,0 +1,20 @@
+{
+    ...
+}: {
+    dotfiles.self.forceEnable = true;
+
+    programs.ssh.knownHosts = {
+        "github/ed25519" = {
+            hostNames = [ "github.com" ];
+            publicKey = "${builtins.readFile ./keys/github.ed25519.pub}";
+        };
+        "github/sha2" = {
+            hostNames = [ "github.com" ];
+            publicKey = "${builtins.readFile ./keys/github.sha2.pub}";
+        };
+        "github/rsa" = {
+            hostNames = [ "github.com" ];
+            publicKey =  "${builtins.readFile ./keys/github.rsa.pub}";
+        };
+    };
+}
