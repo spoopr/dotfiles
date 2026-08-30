66c84320af9e97375c949e763575ab75f15fa14f
spoopr <spoopr@nootr.dev>
2026-08-30 22:23:25 +0000

    convert wireguard to systemd-networkd
    chore
    i have to build impurely in order to use this bottlecap, for now

--- config/wireguard/default.nix
+++ config/wireguard/default.nix
@@ -1,46 +1,89 @@
 {
-  pkgs,
   dots,
+  pkgs,
   ...
 }: let
     inherit (dots.args) secrets;
 in {
-    dotfiles.args.secrets.enable = true;
+    dotfiles = {
+        args.secrets.enable = true;
+        
+        network.enable = true;
+    };
+
+    # see `man systemd.netdev`
+    age.secrets."wireguard" = {
+        file = /nix/persist/repos/secrets/hosts/awa/vpn/wireguard/awa.age;
+        mode = "0440";
+        owner = "root";
+        group = "systemd-network";
+    };
 
     boot.kernelModules = [
-        "wireguard" 
+        "wireguard"
         "xt_addrtype"
         "xt_comment"
         "xt_mark"
         "xt_connmark"
         "xt_conntrack"
-        "ipt_REJECT"
     ];
 
-    networking = {
-        firewall.allowedUDPPorts = [ 51820 ];
+    environment.systemPackages = with pkgs; [
+        wireguard-tools
+    ];
 
-        wg-quick.interfaces = {
-            awa = {
-                autostart = false;
+    networking.firewall = {
+        allowedUDPPorts = [ 51820 ];
+        checkReversePath = "loose";
+    };
 
-                address = [ "10.2.0.2/32" ];
-                dns = [ "10.2.0.1" ];
-                listenPort = 51820; 
+    systemd.network = {
+        networks."50-wg0" = {
+            matchConfig.Name = "wg0";
+            
+            address = [ "10.2.0.2/32" ];
 
-                privateKeyFile = secrets.vpn.wireguard.awa;
+            domains = [ "~." ];
+            dns = [ "10.2.0.1" ];
+            networkConfig.DNSDefaultRoute = true;
 
-                postUp = "wg set awa fwmark 51820 && ${pkgs.iptables}/bin/iptables -I OUTPUT ! -o awa -m mark ! --mark $(wg show awa fwmark) -m addrtype ! --dst-type LOCAL -j REJECT";
-                preDown = "${pkgs.iptables}/bin/iptables -D OUTPUT ! -o awa -m mark ! --mark $(wg show awa fwmark) -m addrtype ! --dst-type LOCAL -j REJECT";
 
-                peers = [
-                    {
-                        publicKey = "7FslkahrdLwGbv4QSX5Cft5CtQLmBUlpWC382SSF7Hw=";
-                        allowedIPs = [ "0.0.0.0/0" ];
-                        endpoint = "185.159.156.37:51820";
-                    }
-                ];
-            };
-        }; 
+            routingPolicyRules = [
+                {
+                    Family = "ipv4";
+                    InvertRule = true;
+                    FirewallMark = 51820;
+                    Table = 1000;
+                    Priority = 10;
+                }
+                {
+                    To = "185.159.156.37/32";
+                    Table = "main";
+                    Priority = 5;
+                }
+            ];
+        };
+
+        netdevs."50-wg0" = {
+             netdevConfig = {
+                 Kind = "wireguard";
+                 Name = "wg0";
+             };
+
+             wireguardConfig = {
+                ListenPort = 51820;
+                PrivateKeyFile = "/run/agenix/wireguard";
+                FirewallMark = 51820;
+             };
+
+             wireguardPeers = [
+                {
+                    PublicKey = "7FslkahrdLwGbv4QSX5Cft5CtQLmBUlpWC382SSF7Hw=";
+                    AllowedIPs = [ "0.0.0.0/0" ];
+                    Endpoint = "185.159.156.37:51820";
+                    RouteTable = 1000;
+                }
+             ];
+        };
     };
 }
