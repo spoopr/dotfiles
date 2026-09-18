63da53731aa99f2c01c1d922e63c111101baf962
spoopr <spoopr@nootr.dev>
2026-09-18 14:42:02 +0000

    remove openvpn service and add ad-hoc command
    chore / feat

--- config/openvpn/default.nix
+++ config/openvpn/default.nix
@@ -4,33 +4,18 @@
   dots,
   ...
 }: let
-    mkService = name: { config, auth }: {
-        description = ''OpenVPN for ${lib.strings.removePrefix "openvpn-" name}'';
-
-        path = with pkgs; [
-            iptables
-            iproute2
-            nettools
-        ];
-
-        serviceConfig = {
-            ExecStart = "@${pkgs.openvpn}/bin/openvpn --suppress-timestamps --config ${config} --auth-user-pass ${auth} --up ${./update-resolv-conf} --down ${./update-resolv-conf}";
-            Restart = "always";
-            Type = "notify";
-        };
-    };
-
-    inherit (dots.args) secrets;
-
+    inherit (dots.inputs) wrappers;
 in {
-    dotfiles.args.secrets.enable = true;
-
     boot.kernelModules = [ "tun" ];
 
-    systemd.services = builtins.mapAttrs mkService {
-        "openvpn-awa" = {
-            config = secrets.vpn.openvpn."awa.tcp.ovpn";
-            auth = secrets.vpn.openvpn.spoopr;
-        };
-    };
+    networking.firewall.allowedUDPPorts = [ 1337 ];
+
+    environment.systemPackages = {
+        inherit pkgs;
+        package = pkgs.openvpn;
+
+        flags."--config" = ./disable-ipv6.conf;
+    }
+        |> wrappers.lib.wrapPackage
+        |> lib.singleton;
 }
